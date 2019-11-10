eee1714e794a3582d628270e44bdfc082c1abb65
PycT <rustem.zakiev@gmail.com>
2019-11-10 20:12:27 +0300

    rearranged files to fix the package publishing issue

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ from io import open
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -34,7 +34,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='Rhythmic',  # Required
+    name='rhythmic',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.7',  # Required
+    version='0.0.8',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -79,11 +79,11 @@ setup(
 
     # This should be your name or the name of the organization which owns the
     # project.
-    author='rustem.zakiev@gmail.com',  # Optional
+    author='Rustem Zakiev',  # Optional
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    #author_email='pypa-dev@googlegroups.com',  # Optional
+    author_email='rustem.zakiev@gmail.com',  # Optional
 
     # Classifiers help users find your project by categorizing it.
     #
